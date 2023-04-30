105ae0e3df539d0059324e23900125ab8325b23b
NicuAndBodea <sirghinicolae231@gmail.com>
2023-04-30 22:00:59 +0300

    multilanguage in web with react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useTranslation, initReactI18next } from "react-i18next";
+import i18next from 'i18next'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.js';
+import 'flag-icons/css/flag-icons.min.css'
 
 function App() {
+
+  const languages = [
+    {
+      code: 'fr',
+      name: 'Français',
+      country_code: 'fr',
+    },
+    {
+      code: 'en',
+      name: 'English',
+      country_code: 'gb',
+    },
+    {
+      code: 'ar',
+      name: 'العربية',
+      country_code: 'sa',
+    },
+  ]
+
+  const { t } = useTranslation();
+
+  const myBirthday = new Date('1999-02-07')
+  const timeDifference = new Date() - myBirthday
+  const numbers_of_days = Math.floor(timeDifference / (1000 * 60 * 60 * 24))
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className="container">
+      <div className="d-flex flex-column align-items-start">
+        <h1 className="fonst-wigth-normal mb-3">{t('welcome_message')}</h1>
+        <p>{t('I_have_this_numbers_of_days', { numbers_of_days })}</p>
+      </div>
+      <div class="dropdown">
+        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Dropdown button
+        </button>
+        <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+          {languages.map(({ code, name, country_code }) => (
+            <li key={country_code} className="dropdown-item">
+              <button onClick={()=>i18next.changeLanguage(code)} className="dropdown-item">
+                <span className={`fi fi-${country_code}`}></span>
+                {name}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,28 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import i18n from "i18next";
+import { useTranslation, initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector"
+import HttpApi from 'i18next-http-backend';
+
+
+i18n
+  .use(initReactI18next)
+  .use(LanguageDetector)
+  .use(HttpApi)
+  .init({
+    supportedLngs:['en','fr','ar'],
+    fallbackLng: "en",
+    detection: {
+      order: ['cookie', 'htmlTag', 'localStorage', 'path', 'subdomain'],
+      caches: ['cookie', 'localStorage'],
+    },
+    backend: {
+      loadPath: '/assets/locales/{{lng}}/translation.json',
+    },
+    debug:true
+  });
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
